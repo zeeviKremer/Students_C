548712fafba11b9d9160e8b5b6bdcdb4a95b213c
zeeviKremer <76267341+zeeviKremer@users.noreply.github.com>
2022-05-04 05:48:07 +0300

    Update query.h

--- query.h
+++ query.h
@@ -1,3 +1,22 @@
 
+
+typedef enum {
+
+	equal, bigger, smaller, biggerEqual, smallerEqual, notEqual
+
+} enumOperators;
+
+
+student* getIndexToInsertQuery(student* head, student* ptrNode);
+void addTolistQuery(student** head, student* ptrPrv, student* newStud);
+void updateStudentQuery(student** head, student* ptrNode, student* newStud);
+int select_firstname(student* head, int operator, char* data);
+int select_secondname(student* head, int operator, char* data);
+int select_id(student* head, int operator, char* data);
+int select_clanguage(student* head, int operator, char* data);
+int select_computernetworks(student* head, int operator, char* data);
+int select_csfundamentals(student* head, int operator, char* data);
+int select_average(student* head, int operator, float* data);
+int genericSelect(student* head, int field, int operator,void* data);
 void runQueries(student* head);
-int checkQweri(char* query, student** head);
+int checkQuery(char* query, student** head);

192b8d7dd06ef955e7891e3f966f0d16fb71ca85
zeeviKremer <76267341+zeeviKremer@users.noreply.github.com>
2022-05-08 21:38:10 +0300

    Update validations.h

--- validations.h
+++ validations.h
@@ -1,15 +1,16 @@
 
-int isChars(char* line);
-int properId(char* line);
-int checkCoursName(char* line);
-int isScore(char* line);
+int isChars(char* row);
+int properId(char* row);
+int checkCoursName(char* row);
+int isScore(char* row);
 int checkNamesData(char* data);
 int checkIdData(char* data);
 int checkScoresData(char* data);
 int checkAverageData(char* data);
-int checkFildName(char* queryRow, char* fild, int flag);
+int checkFildName(char* queryRow, char* field, int flag);
 int checkOperator(char* queryRow, char* operator,int flag);
-int checkData(char* queryRow, char* fild, char* data);
+int checkData(char* queryRow, char* field, char* data);
 int validationRow(char* Row);
-int validetionSelectQuery(char* queryLine, char* field, char* operator,void* data);
-int validationSetQuery(char* line1);
+int validetionSelectQuery(char* queryRow, char* field, char* operator,void* data);
+int validationSetQuery(char* queryRow);
+int validationDeleteQuery(char* queryRow, char* data);

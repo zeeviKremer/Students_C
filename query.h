959a7c20c2e97dec4fd36a96fabb8d2b855399e7
zeeviKremer <76267341+zeeviKremer@users.noreply.github.com>
2022-05-08 21:34:52 +0300

    Update query.h

--- query.h
+++ query.h
@@ -17,6 +17,8 @@ int select_clanguage(student* head, int operator, char* data);
 int select_computernetworks(student* head, int operator, char* data);
 int select_csfundamentals(student* head, int operator, char* data);
 int select_average(student* head, int operator, float* data);
+void deleteStudent(student** head, char* data);
 int genericSelect(student* head, int field, int operator,void* data);
+void printMenu();
 void runQueries(student* head);
 int checkQuery(char* query, student** head);

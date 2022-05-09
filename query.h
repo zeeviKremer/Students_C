b4a734dbee6f26366c660e662d33af2394c7f0cb
zeeviKremer <76267341+zeeviKremer@users.noreply.github.com>
2022-05-09 06:26:18 +0300

    Update query.h

--- query.h
+++ query.h
@@ -8,8 +8,8 @@ typedef enum {
 
 
 student* getIndexToInsertQuery(student* head, student* ptrNode);
-void addTolistQuery(student** head, student* ptrPrv, student* newStud);
-void updateStudentQuery(student** head, student* ptrNode, student* newStud);
+void addTolistQuery(studManagar* studentsManager, student* ptrPrv, student* newStud);
+void updateStudentQuery(studManagar* studentsManager, student* ptrNode, student* newStud);
 int select_firstname(student* head, int operator, char* data);
 int select_secondname(student* head, int operator, char* data);
 int select_id(student* head, int operator, char* data);
@@ -17,8 +17,8 @@ int select_clanguage(student* head, int operator, char* data);
 int select_computernetworks(student* head, int operator, char* data);
 int select_csfundamentals(student* head, int operator, char* data);
 int select_average(student* head, int operator, float* data);
-void deleteStudent(student** head, char* data);
-int genericSelect(student* head, int field, int operator,void* data);
+void deleteStudent(studManagar* studentsManager, char* data);
+int genericSelect(studManagar* studentsManager, int field, int operator,void* data);
 void printMenu();
-void runQueries(student* head);
-int checkQuery(char* query, student** head);
+void runQueries(studManagar* studentsManager);
+int checkQuery(char* query, studManagar* studentsManager);

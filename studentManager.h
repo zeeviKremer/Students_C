18eef8a6aed7f86d9df794f70b0634b15004a148
zeeviKremer <76267341+zeeviKremer@users.noreply.github.com>
2022-05-11 02:57:55 +0300

    Update studentManager.h

--- studentManager.h
+++ studentManager.h
@@ -1,8 +1,12 @@
 
-#define MAX_LINE  255
+#define MAX_ROW  255
 #define SUM_COURSES  3
 #define FILE_NAME "students.csv"
 #define NOT_TESTED  -1
+#define MOMORY_ERROR "    memory error cannot locate memory!\n"
+#define OPEN_FILE_ERROR "    Cannot open the file!\n"
+#define EMPTY_FILE "    The file is empty!\n"
+#define ROW_TOO_LONG(row) "    Error in file, row number %d is too long!\n",row
 
 
 typedef struct student {
@@ -33,20 +37,20 @@ typedef enum {
 
 
 
-studManagar* readFile(char* fileName);
-student* createNode(char* row);
+student* createStudent(char* row);
 int existInList(studManagar* studentsManager, student* stud);
 student* getPtrToNode(studManagar* studentsManager, student* newStud);
 void updateStudent(student* ptrNode, student* newStud);
 student* getIndexToInsert(studManagar* studentsManager,student* newStud);
 void addTolist(studManagar* studentsManager, student* ptrPrv, student* newStud);
 void updateMark(student* newStud, student* ptrNode);
+studManagar* readFile(char* fileName);
 int updateFile(studManagar* studentsManager);
+void printError(int countRows, int checkSyntax);
 void printTitle();
 void printFuter();
 void printNode(student* stud);
 void printList(studManagar* studentsManager);
-void printError(int countRows,int checkSyntax);
 void getId(char* field,char* operator,int* fieldId, int* operatorId);
 void freeStudent(student* ptrStud);
 void freeList(studManagar* studentsManager);
